b381a09bfcd43386aa34fbea13cffc4a8786a3d2
kgelb4 <32882235+kgelb4@users.noreply.github.com>
2017-12-05 00:22:26 -0500

    Pseudo_Code Final Project

---
+++ JS/main.js
@@ -0,0 +1,3 @@
+// When the user clicks the hamburger icon .hamburger add the class active (.active) to the nav (nav) to slide the Sidebar
+// When the user clicks .close (x), removeClass
+// When the user clicks (.click) the button ('button') cars (.cars), hide (hide()) "SUVS" abd "Trucks/Minivans"
